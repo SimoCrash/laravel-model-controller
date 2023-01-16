94b72e23ae9c6b17cb354d580f44482fe79e95c3
SimoCrash <simone.crescenzi@hotmail.com>
2023-01-16 18:44:03 +0100

    es.completo + Bonus

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Movies DB</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+    <h1>Movies</h1>
     <ul>
         @foreach ($movies as $movie)    
         <li>Titolo: {{$movie->title}},
