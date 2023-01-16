a30305fe69b3e8758b989f2d0c94ebd77b696088
SimoCrash <simone.crescenzi@hotmail.com>
2023-01-16 18:04:04 +0100

    punto 7.es_completo

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Movie;
 
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+
+        $movies = Movie::all();
+        return view('guest.index', compact('movies'));
+    }
 }
